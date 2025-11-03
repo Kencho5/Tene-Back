663336d97ab601e58ef31d3c026241bccdb05d3e
Kencho5 <giokenchadze@gmail.com>
2025-11-03 11:01:20 +0400

    fix: route id

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
 mod health;
 mod product;
 
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
 
 use crate::AppState;
 
@@ -9,5 +9,5 @@ pub fn create_router() -> Router<AppState> {
     Router::new()
         .route("/health", get(health::health_check))
         .route("/health/ready", get(health::readiness_check))
-        .route("/product/:id", get(product::get_product))
+        .route("/product/{id}", get(product::get_product))
 }
