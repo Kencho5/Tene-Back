b685f4cfc4629b2030cf000b1d7c082571d5d52b
Kencho5 <giokenchadze@gmail.com>
2025-12-27 23:35:44 +0400

    add: error handling

--- src/routes/user_addresses.rs
+++ src/routes/user_addresses.rs
@@ -13,11 +13,12 @@ pub async fn add_address(
     Extension(claims): Extension<Claims>,
     Json(payload): Json<UserAddress>,
 ) -> Result<Json<UserAddress>> {
-    let user_id = claims.sub.parse::<i32>()
+    let user_id = claims
+        .sub
+        .parse::<i32>()
         .map_err(|_| AppError::Unauthorized("Unauthorized".to_string()))?;
 
     let address = add_user_address(&state.db, user_id, payload).await?;
 
     Ok(Json(address))
 }
-
