47baaccdb2de5b8d304c5387d8244c334f9078ab
Kencho5 <giokenchadze@gmail.com>
2026-05-27 19:31:56 +0400

    update: db connection settings

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -1,10 +1,22 @@
 use crate::{config::DatabaseConfig, error::Result};
-use sqlx::{PgPool, postgres::PgPoolOptions};
+use sqlx::{
+    PgPool,
+    postgres::{PgConnectOptions, PgPoolOptions},
+};
+use std::str::FromStr;
+use std::time::Duration;
 
 pub async fn create_pool(config: &DatabaseConfig) -> Result<PgPool> {
+    let opts = PgConnectOptions::from_str(&config.url)?.statement_cache_capacity(0);
+
     let pool = PgPoolOptions::new()
         .max_connections(config.max_connections)
-        .connect(&config.url)
+        .min_connections(0)
+        .acquire_timeout(Duration::from_secs(5))
+        .idle_timeout(Some(Duration::from_secs(60 * 2)))
+        .max_lifetime(Some(Duration::from_secs(60 * 5)))
+        .test_before_acquire(false)
+        .connect_with(opts)
         .await?;
 
     sqlx::migrate!()
